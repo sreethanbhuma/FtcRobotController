caf779c1f9057d0088371408cd678cf563694ba9
sreethanbhuma <sreethan.bhuma@gmail.com>
2026-03-01 15:10:54 -0500

    Create VariablePractice.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VariablePractice.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class VariablePractice extends OpMode {
+    public void init() {
+        int teamNumber = 27136;
+        double motorSpeed = 0.75;
+        boolean clawClosed = true;
+        String teamName = "SharkBots";
+        int motorAngle = 90;
+
+        telemetry.addData("Team number", teamNumber);
+        telemetry.addData("Motor speed", motorSpeed);
+        telemetry.addData("Claw closed", clawClosed);
+        telemetry.addData("Name", teamName);
+        telemetry.addData("Motor angle", motorAngle);
+    }
+
+
+    @Override
+    public void loop() {
+
+    }
+}
