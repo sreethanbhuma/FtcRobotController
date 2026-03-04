47bd3ae76de49e03b632e014deeb58f31678b1e6
sreethanbhuma <sreethan.bhuma@gmail.com>
2026-03-04 17:55:00 -0500

    Create IfPractice.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IfPractice.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+@Disabled
+@TeleOp
+public class IfPractice extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+        double motorSpeed = gamepad1.left_stick_y;
+
+        if (!gamepad1.a) {
+            motorSpeed *= 0.5;
+        }
+
+        telemetry.addData("Left Sick Value", motorSpeed);
+    }
+}
+
+
+/*
+
+AND - &&  if(leftY < 0.5 && leftY > 0)
+OR - ||   if(leftY < 0 || rightY < 0)
+NOT - !   if(!clawClosed
+
+
+ */
